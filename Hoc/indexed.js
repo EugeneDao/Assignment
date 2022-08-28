787eb0b7c71e141be05725fcc3b1103a620cb115
Đặng Trần Phương Ngân <dangtranphuongngan@angs-MacBook-Pro.local>
2022-08-29 00:15:49 +0700

    first commit

---
+++ Hoc/indexed.js
@@ -0,0 +1,26 @@
+const array = [];
+array.push(1);
+console.log(array);
+// arrow function
+function sum(a, b) {
+    return a + b;
+};
+// expression function support with hoisting
+const multil= function(a, b) {
+    return a*b;
+}
+// anonymous function not support with hoisting
+const sub = (a, b)=>{
+    return a - b;
+}
+// arrow function not support with hoisting
+// template literal
+const myName = 'Mindx';
+
+import math from myMath.js;
+console.log(math.sum(5,6)); 
+// concate array dùng để nối 2 array
+// ...array, ...object dùng để trải phẳng
+Math.max = số to nhất
+
+git add remote.origin dùng để liên kết local
---
+++ Hoc/myMath.js
@@ -0,0 +1,13 @@
+export const sum = (a,b) =>{
+return a + b;
+}
+export const sub = (a,b) =>{
+    return a - b;
+}
+export const version = "1.0";
+const math = {
+    sum: sum,
+    sub: sub,
+    version: version,
+}
+export default math;
